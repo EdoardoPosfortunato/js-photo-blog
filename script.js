b4ff7f1777d6b372a3f48689b01fbd1c7618f14c
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-05-13 11:48:03 +0200

    feat: hoverlay

--- script.js
+++ script.js
@@ -4,40 +4,29 @@ const bacheca = document.querySelector('.bacheca')
 let creaCarta = (array) => {
     let cardString = "";
 
-        array.forEach((element) => {
-            const { url, title } = element;
-            console.log(url, title);
+    array.forEach((element) => {
+        const { url, title } = element;
+        // console.log(url, title);
 
-            cardString += `<div class="card">
+        cardString += `<div class="card">
+                <img id="puntina" src="./img/pin.svg" alt="">
                 <img src=${url} alt="">
                 <div id="testo">${title}</div>
             </div>`})
-        }
+
+    return cardString
+}
 
 
-    axios
+axios
     .get("https://lanciweb.github.io/demo/api/pictures/")
     .then((resp) => {
 
         let data = resp.data;
-        
-        let cardString = "";
 
-        data.forEach((element) => {
-            const { url, title } = element;
-            console.log(url, title);
+        let cardString = creaCarta(data)
 
-            cardString += `<div class="card">
-            <img id="puntina" src="./img/pin.svg" alt="">
-                <img src=${url} alt="">
-                <div id="testo">${title}</div>
-            </div>`
-        }
-
-        )
         bacheca.innerHTML = cardString
-
-
     })
 
 
